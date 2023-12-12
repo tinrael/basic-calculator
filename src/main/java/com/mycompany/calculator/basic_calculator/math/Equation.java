b0663d47867ddd5a1b772da7fb33573c165a55e1
Tymur Krasnopir <krasnopir.std@gmail.com>
2023-12-12 19:29:04 +0200

    Add an Equation class

---
+++ src/main/java/com/mycompany/calculator/basic_calculator/math/Equation.java
@@ -0,0 +1,21 @@
+package com.mycompany.calculator.basic_calculator.math;
+
+import java.util.Objects;
+
+public class Equation {
+	private final static double DELTA = 1e-9;
+	
+	private Expression lhs;
+	private Expression rhs;
+	
+	Equation(String equation) {
+		Objects.requireNonNull(equation);
+		String[] expressions = equation.split("=", 2);
+		this.lhs = new Expression(expressions[0]);
+		this.rhs = new Expression(expressions[1]);
+	}
+	
+	public boolean isRoot(double x) {
+		return ((lhs.evaluate(x) - rhs.evaluate(x)) <= DELTA);
+	}
+}
