6832dcd2717fff8d5c9722a80fd26756e02634d5
Tymur Krasnopir <krasnopir.std@gmail.com>
2023-12-12 19:09:31 +0200

    Add an Expression class

---
+++ src/main/java/com/mycompany/calculator/basic_calculator/math/Expression.java
@@ -0,0 +1,24 @@
+package com.mycompany.calculator.basic_calculator.math;
+
+import java.util.Objects;
+
+import com.mycompany.calculator.basic_calculator.Calculator;
+
+public class Expression {
+	private String[] postfix;
+	
+	Expression(String expression) {
+		Objects.requireNonNull(expression);
+		this.postfix = Calculator.convertInfixToPostfix(Tokenizer.tokenize(expression));
+	}
+	
+	public double evaluate(double x) {
+		String[] tokens = new String[this.postfix.length];
+		
+		for (int i = 0; i < this.postfix.length; i++) {
+			tokens[i] = this.postfix[i].replace("x", String.valueOf(x));
+		}
+		
+		return Calculator.evaluatePostfix(tokens);
+	}
+}
