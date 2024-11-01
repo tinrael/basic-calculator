34b0c1ebfa442a9a33de8e6081dfb481e4578532
Tymur Krasnopir <krasnopir.std@gmail.com>
2024-11-01 19:07:21 +0200

    Add the AppTest class

---
+++ src/test/java/com/mycompany/calculator/basic_calculator/AppTest.java
@@ -0,0 +1,20 @@
+package com.mycompany.calculator.basic_calculator;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
